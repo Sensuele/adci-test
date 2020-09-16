be73d775b992d1a590f2f108d2881e07c717a7f9
Евгений Дружняев <druzhniaev@gmail.com>
2020-09-16 22:24:41 +0600

    check

--- js/script.js
+++ js/script.js
@@ -155,4 +155,9 @@ window.addEventListener('DOMContentLoaded', () => {
     body.classList.toggle('hidden');
   });
   
+
+  window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
 });
